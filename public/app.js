a854256882e3260de0991590e38f3ddc46923152
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-10-16 10:47:29 -0400

    Installed synaptic, created neural network

---
+++ public/app.js
@@ -0,0 +1,49 @@
+//var synaptic = require('synaptic');
+var Neuron = synaptic.Neuron;
+var Layer = synaptic.Layer;
+var Network = synaptic.Network;
+var Trainer = synaptic.Trainer;
+var Architect = synaptic.Architect;
+
+// create the network
+var inputLayer = new Layer(2);
+var hiddenLayer = new Layer(3);
+var outputLayer = new Layer(1);
+
+inputLayer.project(hiddenLayer);
+hiddenLayer.project(outputLayer);
+
+var myNetwork = new Network({
+	input: inputLayer,
+	hidden: [hiddenLayer],
+	output: outputLayer
+});
+
+// train the network
+var learningRate = 0.3;
+for (var i = 0; i < 20000; i++)
+{
+	// 0,0 => 0
+	myNetwork.activate([0,0]);
+	myNetwork.propagate(learningRate, [0]);
+
+	// 0,1 => 1
+	myNetwork.activate([0,1]);
+	myNetwork.propagate(learningRate, [1]);
+
+	// 1,0 => 1
+	myNetwork.activate([1,0]);
+	myNetwork.propagate(learningRate, [1]);
+
+	// 1,1 => 0
+	myNetwork.activate([1,1]);
+	myNetwork.propagate(learningRate, [0]);
+  console.log(i);
+}
+
+
+// test the network
+console.log(myNetwork.activate([0,0])); // [0.015020775950893527]
+console.log(myNetwork.activate([0,1])); // [0.9815816381088985]
+console.log(myNetwork.activate([1,0])); // [0.9871822457132193]
+console.log(myNetwork.activate([1,1])); // [0.012950087641929467]
